a588e829cfe394fc0e52c4b6de68be27988bf1e8
MonikaJhingan <jhinganmonika750@gmail.com>
2021-12-12 14:21:52 +0530

    initialcommit

---
+++ at.js
@@ -0,0 +1,16 @@
+const inputs = document.querySelectorAll(".angle-input");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
+const outputEl = document.querySelector("#output");
+
+function calculateArea(side1,side2){
+    const triangleArea = 1/2*(side1 * side2);
+    return triangleArea;
+
+}
+
+function isTriangle(){
+   const areaOfTriangle =  calculateArea((Number(inputs[0].value)),(Number(inputs[1].value)));
+   outputEl.innerText ="Area of a triangle using heron's formula is" + areaOfTriangle +"units";
+}  
+isTriangleBtn.addEventListener("click",isTriangle);
+
